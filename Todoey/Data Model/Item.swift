8d6270e1718f08d76298281c0fe9d85355e5a5d8
Gerald Slomka <geraldslomka@geralds-imac.local>
2019-03-18 23:00:34 -0400

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Gerald Slomka on 3/18/19.
+//  Copyright © 2019 Gerald Slomka. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
